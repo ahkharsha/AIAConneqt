8ef010ed119feed3e993728f18c32b22ffeb5781
Harsha Kumar <ahkharsha@gmail.com>
2024-10-30 21:43:34 +0530

    Minor fix

--- client/context/constants.js
+++ client/context/constants.js
@@ -1,198 +1,198 @@
-import { ethers } from "ethers";
-import Web3Modal from "web3modal";
-import axios from "axios";
-import dotenv from "dotenv";
-import socialMediaDapp from "./SocialMediaDapp.json";
+  import { ethers } from "ethers";
+  import Web3Modal from "web3modal";
+  import axios from "axios";
+  import dotenv from "dotenv";
+  import socialMediaDapp from "./SocialMediaDapp.json";
 
-dotenv.config();
+  dotenv.config();
 
-export const CONTRACT_ABI = socialMediaDapp.abi;
-export const CONTRACT_ADDRESS = "0x73641118e93Be1Ed8cE170E17E564Fa76b43a8fb";
+  export const CONTRACT_ABI = socialMediaDapp.abi;
+  export const CONTRACT_ADDRESS = "0x73641118e93Be1Ed8cE170E17E564Fa76b43a8fb";
 
-// PINATA KEYS from .env
-export const PINATA_API_KEY = process.env.NEXT_PUBLIC_PINATA_API_KEY;
-export const PINATA_SECRET_KEY = process.env.NEXT_PUBLIC_PINATA_SECRET_KEY;
+  // PINATA KEYS from .env
+  export const PINATA_API_KEY = process.env.NEXT_PUBLIC_PINATA_API_KEY;
+  export const PINATA_SECRECT_KEY = process.env.NEXT_PUBLIC_PINATA_SECRET_KEY;
 
-// NETWORK
-const networks = {
-  polygon_amoy: {
-    chainId: `0x${Number(80002).toString(16)}`,
-    chainName: "Polygon Amoy",
-    nativeCurrency: {
-      name: "MATIC",
-      symbol: "MATIC",
-      decimals: 18,
+  // NETWORK
+  const networks = {
+    polygon_amoy: {
+      chainId: `0x${Number(80002).toString(16)}`,
+      chainName: "Polygon Amoy",
+      nativeCurrency: {
+        name: "MATIC",
+        symbol: "MATIC",
+        decimals: 18,
+      },
+      rpcUrls: ["https://rpc-amoy.polygon.technology/"],
+      blockExplorerUrls: ["https://www.oklink.com/amoy"],
     },
-    rpcUrls: ["https://rpc-amoy.polygon.technology/"],
-    blockExplorerUrls: ["https://www.oklink.com/amoy"],
-  },
-  aia_mainnet: {
-    chainId: `0x${Number(1319).toString(16)}`,
-    chainName: "AIA Mainnet",
-    nativeCurrency: {
-      name: "AIA",
-      symbol: "AIA",
-      decimals: 18,
+    aia_mainnet: {
+      chainId: `0x${Number(1319).toString(16)}`,
+      chainName: "AIA Mainnet",
+      nativeCurrency: {
+        name: "AIA",
+        symbol: "AIA",
+        decimals: 18,
+      },
+      rpcUrls: ["https://aia-dataseed1.aiachain.org"],
+      blockExplorerUrls: ["https://www.aiascan.com/"],t
     },
-    rpcUrls: ["https://aia-dataseed1.aiachain.org"],
-    blockExplorerUrls: ["https://www.aiascan.com/"],t
-  },
-  aia_testnet: {
-    chainId: `0x${Number(1320).toString(16)}`,
-    chainName: "AIA Testnet",
-    nativeCurrency: {
-      name: "AIA",
-      symbol: "AIA",
-      decimals: 18,
+    aia_testnet: {
+      chainId: `0x${Number(1320).toString(16)}`,
+      chainName: "AIA Testnet",
+      nativeCurrency: {
+        name: "AIA",
+        symbol: "AIA",
+        decimals: 18,
+      },
+      rpcUrls: ["https://aia-dataseed1-testnet.aiachain.org/"],
+      blockExplorerUrls: ["https://testnet.aiascan.com/"],t
     },
-    rpcUrls: ["https://aia-dataseed1-testnet.aiachain.org/"],
-    blockExplorerUrls: ["https://testnet.aiascan.com/"],t
-  },
-  polygon_mumbai: {
-    chainId: `0x${Number(80001).toString(16)}`,
-    chainName: "Polygon Mumbai",
-    nativeCurrency: {
-      name: "MATIC",
-      symbol: "MATIC",
-      decimals: 18,
+    polygon_mumbai: {
+      chainId: `0x${Number(80001).toString(16)}`,
+      chainName: "Polygon Mumbai",
+      nativeCurrency: {
+        name: "MATIC",
+        symbol: "MATIC",
+        decimals: 18,
+      },
+      rpcUrls: ["https://rpc.ankr.com/polygon_mumbai"],
+      blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
     },
-    rpcUrls: ["https://rpc.ankr.com/polygon_mumbai"],
-    blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
-  },
-  polygon: {
-    chainId: `0x${Number(137).toString(16)}`,
-    chainName: "Polygon Mainnet",
-    nativeCurrency: {
-      name: "MATIC",
-      symbol: "MATIC",
-      decimals: 18,
+    polygon: {
+      chainId: `0x${Number(137).toString(16)}`,
+      chainName: "Polygon Mainnet",
+      nativeCurrency: {
+        name: "MATIC",
+        symbol: "MATIC",
+        decimals: 18,
+      },
+      rpcUrls: ["https://rpc.ankr.com/polygon"],
+      blockExplorerUrls: ["https://polygonscan.com/"],
     },
-    rpcUrls: ["https://rpc.ankr.com/polygon"],
-    blockExplorerUrls: ["https://polygonscan.com/"],
-  },
-  bsc: {
-    chainId: `0x${Number(56).toString(16)}`,
-    chainName: "Binance Smart Chain Mainnet",
-    nativeCurrency: {
-      name: "Binance Chain Native Token",
-      symbol: "BNB",
-      decimals: 18,
+    bsc: {
+      chainId: `0x${Number(56).toString(16)}`,
+      chainName: "Binance Smart Chain Mainnet",
+      nativeCurrency: {
+        name: "Binance Chain Native Token",
+        symbol: "BNB",
+        decimals: 18,
+      },
+      rpcUrls: ["https://rpc.ankr.com/bsc"],
+      blockExplorerUrls: ["https://bscscan.com"],
     },
-    rpcUrls: ["https://rpc.ankr.com/bsc"],
-    blockExplorerUrls: ["https://bscscan.com"],
-  },
-  bsc_testnet: {
-    chainId: `0x${Number(97).toString(16)}`,
-    chainName: "BNB Smart Chain Testnet",
-    nativeCurrency: {
-      name: "Binance Chain Native Token",
-      symbol: "BNB",
-      decimals: 18,
+    bsc_testnet: {
+      chainId: `0x${Number(97).toString(16)}`,
+      chainName: "BNB Smart Chain Testnet",
+      nativeCurrency: {
+        name: "Binance Chain Native Token",
+        symbol: "BNB",
+        decimals: 18,
+      },
+      rpcUrls: ["https://bsc-testnet-rpc.publicnode.com"],
+      blockExplorerUrls: ["https://testnet.bscscan.com"],
     },
-    rpcUrls: ["https://bsc-testnet-rpc.publicnode.com"],
-    blockExplorerUrls: ["https://testnet.bscscan.com"],
-  },
-  base_mainnet: {
-    chainId: `0x${Number(8453).toString(16)}`,
-    chainName: "Base Mainnet",
-    nativeCurrency: {
-      name: "ETH",
-      symbol: "ETH",
-      decimals: 18,
+    base_mainnet: {
+      chainId: `0x${Number(8453).toString(16)}`,
+      chainName: "Base Mainnet",
+      nativeCurrency: {
+        name: "ETH",
+        symbol: "ETH",
+        decimals: 18,
+      },
+      rpcUrls: ["https://mainnet.base.org/"],
+      blockExplorerUrls: ["https://bscscan.com"],
     },
-    rpcUrls: ["https://mainnet.base.org/"],
-    blockExplorerUrls: ["https://bscscan.com"],
-  },
-  base_sepolia: {
-    chainId: `0x${Number(84532).toString(16)}`,
-    chainName: "Base Sepolia",
-    nativeCurrency: {
-      name: "ETH",
-      symbol: "ETH",
-      decimals: 18,
+    base_sepolia: {
+      chainId: `0x${Number(84532).toString(16)}`,
+      chainName: "Base Sepolia",
+      nativeCurrency: {
+        name: "ETH",
+        symbol: "ETH",
+        decimals: 18,
+      },
+      rpcUrls: ["https://sepolia.base.org"],
+      blockExplorerUrls: ["https://bscscan.com"],
     },
-    rpcUrls: ["https://sepolia.base.org"],
-    blockExplorerUrls: ["https://bscscan.com"],
-  },
-  localhost: {
-    chainId: `0x${Number(31337).toString(16)}`,
-    chainName: "localhost",
-    nativeCurrency: {
-      name: "GO",
-      symbol: "GO",
-      decimals: 18,
+    localhost: {
+      chainId: `0x${Number(31337).toString(16)}`,
+      chainName: "localhost",
+      nativeCurrency: {
+        name: "GO",
+        symbol: "GO",
+        decimals: 18,
+      },
+      rpcUrls: ["http://127.0.0.1:8545/"],
+      blockExplorerUrls: ["https://bscscan.com"],
     },
-    rpcUrls: ["http://127.0.0.1:8545/"],
-    blockExplorerUrls: ["https://bscscan.com"],
-  },
-  open_campus_codex: {
-    chainId: `0x${Number(656476).toString(16)}`,
-    chainName: "Open Campus Codex",
-    nativeCurrency: {
-      name: "EDU",
-      symbol: "EDU",
-      decimals: 18,
+    open_campus_codex: {
+      chainId: `0x${Number(656476).toString(16)}`,
+      chainName: "Open Campus Codex",
+      nativeCurrency: {
+        name: "EDU",
+        symbol: "EDU",
+        decimals: 18,
+      },
+      rpcUrls: ["https://rpc.open-campus-codex.gelato.digital"],
+      blockExplorerUrls: ["https://opencampus-codex.blockscout.com"],
     },
-    rpcUrls: ["https://rpc.open-campus-codex.gelato.digital"],
-    blockExplorerUrls: ["https://opencampus-codex.blockscout.com"],
-  },
-  neox_testnet: {
-    chainId: `0x${Number(12227332).toString(16)}`,
-    chainName: "NeoX Testnet T4",
-    nativeCurrency: {
-      name: "NeoX",
-      symbol: "GAS",
-      decimals: 18,
+    neox_testnet: {
+      chainId: `0x${Number(12227332).toString(16)}`,
+      chainName: "NeoX Testnet T4",
+      nativeCurrency: {
+        name: "NeoX",
+        symbol: "GAS",
+        decimals: 18,
+      },
+      rpcUrls: ["https://12227332.rpc.thirdweb.com"],
+      blockExplorerUrls: ["https://xexplorer.neo.org/"],
     },
-    rpcUrls: ["https://12227332.rpc.thirdweb.com"],
-    blockExplorerUrls: ["https://xexplorer.neo.org/"],
-  },
-};
+  };
 
-const changeNetwork = async ({ networkName }) => {
-  try {
-    if (!window.ethereum) throw new Error("No crypto wallet found");
-    await window.ethereum.request({
-      method: "wallet_addEthereumChain",
-      params: [
-        {
-          ...networks[networkName],
-        },
-      ],
-    });
-  } catch (err) {
-    console.log(err.message);
-  }
-};
+  const changeNetwork = async ({ networkName }) => {
+    try {
+      if (!window.ethereum) throw new Error("No crypto wallet found");
+      await window.ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [
+          {
+            ...networks[networkName],
+          },
+        ],
+      });
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
-export const handleNetworkSwitch = async () => {
-  const networkName = "aia_testnet";
-  await changeNetwork({ networkName });
-};
+  export const handleNetworkSwitch = async () => {
+    const networkName = "aia_testnet";
+    await changeNetwork({ networkName });
+  };
 
-export const checkIfWalletConnected = async () => {
-  if (!window.ethereum) return console.log("Please Install MetaMask");
-  const network = await handleNetworkSwitch();
-
-  const account = await window.ethereum.request({ method: "eth_accounts" });
-
-  if (account.length) {
-    return account[0];
-  } else {
-    console.log("Please Install MetaMask & Connect, Reload");
-  }
-};
-
-export const connectWallet = async () => {
-  try {
-    if (!window.ethereum) return alert("Please install MetaMask");
+  export const checkIfWalletConnected = async () => {
+    if (!window.ethereum) return console.log("Please Install MetaMask");
     const network = await handleNetworkSwitch();
-    const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
 
-    return accounts[0];
-  } catch (error) {
-    console.log(error);
-  }
-};
+    const account = await window.ethereum.request({ method: "eth_accounts" });
+
+    if (account.length) {
+      return account[0];
+    } else {
+      console.log("Please Install MetaMask & Connect, Reload");
+    }
+  };
+
+  export const connectWallet = async () => {
+    try {
+      if (!window.ethereum) return alert("Please install MetaMask");
+      const network = await handleNetworkSwitch();
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      return accounts[0];
+    } catch (error) {
+      console.log(error);
+    }
+  };
